b1f070823adbe2274fdc61a2dcecc59630d315ed
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2023-03-26 23:55:34 -0300

    add commands and templates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,9 @@ Fail2ban admin panel in flask app
 
 ## [1.0.0] - 2023-03-26
     - Flask app initial version
+
+## [1.0.1] - 2023-03-26
+
+### Added
+    - Api rest for commands fail2ban
+    - Initial template
--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import subprocess
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
-    return "hola mundo"
+    command = ['docker', 'exec', '-it', 'jovial_bhaskara', 'sqlite3',
+               '/var/lib/fail2ban/fail2ban.sqlite3',
+               'select ip,jail from bips']
+    process = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding='UTF-8'
+        )
+    return render_template('index.html', data=process.stdout)
 
 
-@app.route('/fail2ban/show', methods='GET')
-def show():
-    pass
+@app.route('/fail2ban/ban/<ip>', methods=['GET'])
+def add_ban(ip):
+    command = ['fail2ban-client', 'set', 'sshd', ip]
+    process = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding="UTF-8"
+        )
+    return jsonify(result=process.stdout)
 
 
-@app.route('/fail2ban/ban/<ip>', methods='GET')
-def ban():
-    pass
-
-
-@app.route('/fail2ban/unban/<ip>', methods='GET')
-def unban():
-    pass
+@app.route('/fail2ban/unban/<ip>', methods=['GET'])
+def remove_ban(ip):
+    command = ['fail2ban-client', 'unban', ip]
+    process = subprocess.run(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        encoding="UTF-8"
+        )
+    return jsonify(result=process.stdout)
 
 
 if __name__ == '__main__':
